aa8bd350424683cd5326f343573c8fa95e7c35c1
lucasamaraloliveira <lucas.amaral.oliveira.silva@gmail.com>
2023-01-14 22:06:07 -0300

    feat: added a button to return to the top

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import Navbar from './components/layout/Navbar'
-import Container from './components/layout/Container'
-import Footer from './components/layout/Footer'
-import HamburgerMenu from './components/layout/Navbar/HamburgerMenu'
+import Navbar from "./components/layout/Navbar";
+import Container from "./components/layout/Container";
+import Footer from "./components/layout/Footer";
+import HamburgerMenu from "./components/layout/Navbar/HamburgerMenu";
+import React, { useRef } from "react";
 
-
-
+import Arrow from "./assets/arrow.png";
 
 function App() {
+  const topRef = useRef(null);
+
+  const handleClick = () => {
+    topRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
+
   return (
     <div>
-      <Navbar/>
-      <HamburgerMenu/>
-      <Container/>
-      <Footer/>
-    </div> 
-  
-  )
+      <button className="top-button " onClick={handleClick}>
+        <img className="btn-topo" src={Arrow} alt="" />
+      </button>
+      <div ref={topRef}>
+        <Navbar />
+        <HamburgerMenu />
+        <Container />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
