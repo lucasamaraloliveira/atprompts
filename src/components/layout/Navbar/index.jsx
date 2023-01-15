09c0ee2e84a4db7c271b4d1606aa93df7fa41aaf
lucasamaraloliveira <lucas.amaral.oliveira.silva@gmail.com>
2023-01-14 21:20:23 -0300

    eat: added smoothie effect when clicking on menu

--- src/components/layout/Navbar/index.jsx
+++ src/components/layout/Navbar/index.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React,{ useEffect, useRef }  from 'react'
 import './Navbar.css'
 
-
 function Navbar() {
-  return (
 
-    
-    
+  const menuLinks = useRef(null);
+
+  useEffect(() => {
+    menuLinks.current = document.querySelectorAll('a');
+    menuLinks.current.forEach(link => {
+      link.addEventListener('click', e => {
+        e.preventDefault();
+        const sectionId = e.target.getAttribute('href');
+        document.querySelector(sectionId).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+  }, []);
+
+  return (
     <div className= 'navbar'>
-      
         <ul>
           <li><a href="#"> Início</a></li>
           <li><a href="#About"> Sobre</a></li>
           <li><a href="#Prompts"> Prompts</a></li>
-
         </ul>
-       
-
     </div>
-    
   )
 }
 
