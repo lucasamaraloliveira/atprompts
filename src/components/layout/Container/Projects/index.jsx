85bc1f8330c826dbd2a945a6fcac9dee2fdd84fa
Lucas Amaral Oliveira da Silva <lucas.amaral.oliveira.silva@gmail.com>
2023-01-16 11:05:18 -0300

    style: adjusted footer

--- src/components/layout/Container/Projects/index.jsx
+++ src/components/layout/Container/Projects/index.jsx
@@ -10,6 +10,7 @@ function Projects() {
   const [text5, setText5] = useState("Quero que você atue como um engenheiro de aprendizado de máquina. Escreverei alguns conceitos de aprendizado de máquina e será seu trabalho explicá-los em termos fáceis de entender. Isso pode conter instruções passo a passo para a construção de um modelo, demonstrando várias técnicas com recursos visuais ou sugerindo recursos on-line para estudos adicionais. Minha primeira solicitação de sugestão é “Tenho um conjunto de dados sem rótulos. Qual algoritmo de aprendizado de máquina devo usar?”");
   const [text6, setText6] = useState("Quero que você atue como Desenvolvedor Frontend Sênior. Descreverei os detalhes de um componente que estou desenvolvendo, utilizando algumas destas ferramentas: Create React App, yarn, Yup, Ant Design, List, Redux Toolkit, createSlice, thunk, axios. Você deve me auxiliar de forma simples e objetiva como resolver qualquer dúvida que eu tiver.  Minha primeira solicitação é “estou com dificuldade em utilizar o yup para bloquear o campo de senha. ”");
   const [text7, setText7] = useState("Eu quero que você atue como um gerador de mensagens de commit. Fornecerei informações sobre a tarefa e o prefixo do código da tarefa e gostaria que você gerasse uma mensagem de commit apropriada usando o formato de conventional commit em inglês. Não escreva nenhuma explicação ou outras palavras, apenas responda com a mensagem de confirmação. Meu primeiro pedido é “ajuste de rodapé.”");
+  const [text8, setText8] = useState("Quero que você atue como CEO de uma empresa hipotética. Você será responsável por tomar decisões estratégicas, gerenciar o desempenho financeiro da empresa e representar a empresa para as partes interessadas externas. Você receberá uma série de cenários e desafios para responder e deverá usar seu melhor julgamento e habilidades de liderança para encontrar soluções. Lembre-se de permanecer profissional e tomar decisões que sejam do melhor interesse da empresa e de seus funcionários. Seu primeiro desafio é: “enfrentar uma possível situação de crise em que um recall de produto é necessário. Como você vai lidar com essa situação e que medidas vai tomar para mitigar qualquer impacto negativo na empresa?”");
 
 
   const textareaRef1 = React.useRef(null);
@@ -19,6 +20,7 @@ function Projects() {
   const textareaRef5 = React.useRef(null);
   const textareaRef6 = React.useRef(null);
   const textareaRef7 = React.useRef(null);
+  const textareaRef8 = React.useRef(null);
   return (
     <div>
       
@@ -72,6 +74,13 @@ function Projects() {
                 text={text7}
                 textareaRef={textareaRef7}
                 onChange={e => setText7(e.target.value)} />
+
+            <h3> Atuar como Diretor Executivo</h3>
+            <span>Contribuição de: <a target="_blank" href='https://github.com/lucasamaraloliveira'> @lucasamaral </a> complexidade: media </span>
+                <TextArea 
+                text={text8}
+                textareaRef={textareaRef8}
+                onChange={e => setText8(e.target.value)} />
 
 
             </div>
